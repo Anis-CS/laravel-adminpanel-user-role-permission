517897db297ef0b588ecb5c6e49ad291eb08db69
Anis-CS <anisurrhaman0133@gmail.com>
2026-01-03 14:07:05 +0600

    permission update

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -111,6 +111,7 @@
                     
                     @if($user->can('settings.view') ||
                         $user->can('settings.env_editor.view') ||
+                        $user->can('settings.action_history.index') ||
                         $user->can('settings.user_location.view'))
 
                         <li class="treeview">
